da6d9bce435c09005f0d8e27e08345c4f4fc6edc
CookieCrumbs19212 <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-05-25 02:44:07 +0530

    Renamed Database.java to IvoryDatabase.java

--- IvoryDatabase.java
+++ IvoryDatabase.java
@@ -1,23 +1,23 @@
 import java.io.FileNotFoundException;
 
-public class Database{
+public class IvoryDatabase{
     String[] attributes; // the column headers are called attributes eg. name, ID no., address, etc.
     String[][] table; // the 2 Dimensional array where the data is stored in a structured manner
     int rows, columns; // total number of rows, total number of columns, in the table
     String file_location; // location of the .csv file where the database table is stored
 
-    public Database(int rows, int columns, String file_location)throws FileNotFoundException{
+    public IvoryDatabase(int rows, int columns, String file_location)throws FileNotFoundException{
         this.rows = rows;
         this.columns = columns;
         table = new String[rows][columns];
         this.file_location = file_location;
-    } // constructor Database(rows, columns)
+    } // constructor IvoryDatabase(rows, columns)
 
     // initialize a Database object 
-    public Database(String file_location)throws FileNotFoundException {
+    public IvoryDatabase(String file_location)throws FileNotFoundException {
         this.file_location = file_location;
 
-    } // constructor Database(file_location)
+    } // constructor IvoryDatabase(file_location)
 
     public void sortDataBy(String attribute_name, boolean ascending){
         Sorter.quickSort(table, attribute_name, ascending);
--- Sorter.java
+++ Sorter.java
@@ -6,7 +6,7 @@ class Sorter {
     private int rows; // stores the number of rows in the table, this includes the column header row
     private int attribute_index; // the attribute according to which the table is going to be organized
     
-    public void quickSort(String[][] table, String attribute_name, boolean ascending) {
+    public void sort(String[][] table, String attribute_name, boolean ascending) {
 
         // validating table
         if (table == null || table.length == 0){
@@ -15,11 +15,11 @@ class Sorter {
         
         this.table = table;
         this.rows = table.length;
-        this.attributes = table[0]; // 0th row is column headers, these are stored in attributes
+        this.attributes = table[0]; // 0th row is column headers, attribute names are the column headers
         this.attributes_len = attributes.length;
 
         // validating and getting attribute_index
-        this.attribute_index = getAttributeIndex(attribute_name.toUpperCase());
+        this.attribute_index = getIndexOfAttribute(attribute_name.toUpperCase());
         if(attribute_index == -1){ // getAttributeIndex() returns -1 if the attribute_name does not exist for this table
             return;
         }
@@ -34,7 +34,11 @@ class Sorter {
             quickSortDescending(1, rows-1); // table sorted in descending order
     } // sort()
 
+
+
     /*
+     * START OF STRING DATA TYPE SORTING SECTION
+     *   
      * This method implements in-place quicksort algorithm recursively.
      */
     private void quickSortAscending(int low, int high) {
@@ -66,7 +70,7 @@ class Sorter {
             }
         } // while loop
 
-        // calls quickSort() method recursively
+        // calls quickSortAscending() method recursively
         if (low < index2) {
             quickSortAscending(low, index2);
         }
@@ -114,21 +118,111 @@ class Sorter {
         if (index1 < high)
             quickSortDescending(index1, high);
     } // quickSortDescending()
+    /**
+     * END OF STRING DATA TYPE SORTING SECTION
+     */
+
+
+    /**
+     * START OF INTEGER DATA TYPE SORTING SECTION
+     */
+    private void quickSortAscending(int low, int high, int[] attribute_array) {
+        int index1 = low;
+        int index2 = high;
+
+        // pivot is middle index
+        String pivot = table[low + (high-low)/2][attribute_index];
+
+        // Divide into two arrays
+        while (index1 <= index2) {
+            /**
+             * As shown in above image, In each iteration, we will identify a
+             * number from left side which is greater then the pivot value, and
+             * a number from right side which is less then the pivot value. Once
+             * search is complete, we can swap both numbers.
+             */
+            while (table[index1][attribute_index].compareTo(pivot) < 0) {
+                index1++;
+            }
+            while (table[index2][attribute_index].compareTo(pivot) > 0) {
+                index2--;
+            }
+            if (index1 <= index2) {
+                swap(index1, index2);
+                // move index to next position on both sides
+                index1++;
+                index2--;
+            }
+        } // while loop
+
+        // calls quickSortAscending() method recursively
+        if (low < index2) {
+            quickSortAscending(low, index2);
+        }
+
+        if (index1 < high) {
+            quickSortAscending(index1, high);
+        }
+    } // quickSortAscending()
+
+    /*
+     * This method implements in-place quicksort algorithm recursively.
+     */
+    private void quickSortDescending(int low, int high) {
+        int index1 = low;
+        int index2 = high;
+
+        // pivot is middle index
+        String pivot = table[low + (high-low)/2][attribute_index];
+
+        // Divide into two arrays
+        while (index1 <= index2) {
+            /**
+             * In each iteration, we will identify a number from left side which is 
+             * less than the pivot value, and a number from right side which is greater 
+             * than the pivot value. Once search is complete, we can swap both numbers.
+             */
+            while (table[index1][attribute_index].compareTo(pivot) > 0) {
+                index1++;
+            }
+            while (table[index2][attribute_index].compareTo(pivot) < 0) {
+                index2--;
+            }
+            if (index1 <= index2) {
+                swap(index1, index2);
+                // move index to next position on both sides
+                index1++;
+                index2--;
+            }
+        } // while loop
+
+        // calls quickSortDescending() method recursively
+        if (low < index2)
+            quickSortDescending(low, index2);
+
+        if (index1 < high)
+            quickSortDescending(index1, high);
+    } // quickSortDescending()
+    /**
+     * END OF INTEGER DATA TYPE SORTING SECTION
+     */
+
+    
 
     // method to swap the elements in index1 and index2
     private void swap(int index1, int index2) {
         String[] temp = table[index1];
         table[index1] = table[index2];
         table[index2] = temp;
-    }
+    } // swap()
 
     // method to check if attribute_name exists in this table and return its index value in attributes[]
-    private int getAttributeIndex(String attribute_name){
+    private int getIndexOfAttribute(String attribute_name){
         for(int index = 0 ; index < attributes_len ; index++){
-            if(attribute_name.equals(attributes[index]))
+            if(attribute_name.equalsIgnoreCase(attributes[index]))
                 return index;
         }
         return -1; // attribute_name does not exist in this table
-    } // validateAttribute()
+    } // getIndexOfAttribute()
 
 } // class Sorter
