cb673b55c421a266689dd25530f7147b3ff2d2d9
CookieCrumbs19212 <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-05-25 21:41:36 +0530

    Update IvoryDatabase.java
    IN PROGRESS - Updated IvoryDatabase.java to work with the new Attribute.java Object class

--- IvoryDatabase.java
+++ IvoryDatabase.java
@@ -1,27 +1,53 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import IvoryDBExceptions.*;
 
-public class IvoryDatabase{
-    String[] attributes; // the column headers are called attributes eg. name, ID no., address, etc.
-    String[][] table; // the 2 Dimensional array where the data is stored in a structured manner
+public class IvoryDatabase implements AutoCloseable{
+    /**
+     * Each Attribute object in attributes[] acts like a Column.
+     * Each Attribute object holds the data values for an Attribute in an Object Array.
+     * Hence, the attributes for each "Row Entry" is spread across the several 
+     */
+    Attribute[] attributes;
     int rows, columns; // total number of rows, total number of columns, in the table
-    String file_location; // location of the .csv file where the database table is stored
+    String file_location; // location of the .ivry file where the database table is stored
 
-    public IvoryDatabase(int rows, int columns, String file_location)throws FileNotFoundException{
-        this.rows = rows;
-        this.columns = columns;
-        table = new String[rows][columns];
-        this.file_location = file_location;
+    // initializing an IvoryDatabase object and creating a new .ivry file at "file_location"
+    public IvoryDatabase(int rows, int columns, String file_location) {
+        this.rows = rows; // number of rows (also called "entries") in the database
+        this.columns = columns; // number of columns (also called "attributes") in the database
+        this.file_location = file_location; // file location of the database
+        attributes = new Attribute[columns]; // creating an Attribute Object array (Attribute Objects not initialized yet)
     } // constructor IvoryDatabase(rows, columns)
 
-    // initialize a Database object 
-    public IvoryDatabase(String file_location)throws FileNotFoundException {
-        this.file_location = file_location;
+    // initializing an IvoryDatabase object with an existing .ivry file at "file_location"
+    public IvoryDatabase(String file_location) throws IvoryDatabaseNotFoundException{
+        // validating file_location
+        if (file_location == null || !file_location.endsWith(".ivry")) {
+            throw new IvoryDatabaseNotFoundException(file_location);
+            return;
+        }
+        this.file_location = file_location; // assigning to global variable
+        // 
+        try (BufferedReader br = new BufferedReader(new FileReader(file_location))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                table[row++] = line.split(","); // split up values in the line and store in String array
+            } // while
+
+        } // try
+        catch (FileNotFoundException e) { e.printStackTrace(); }
 
     } // constructor IvoryDatabase(file_location)
 
     public void sortDataBy(String attribute_name, boolean ascending){
-        Sorter.quickSort(table, attribute_name, ascending);
+        
     }
+
+    public void getCell(int row, int column){
+
+    } // getCell()
 
     public void addColumn(String attribute_name){
 
@@ -50,4 +76,20 @@ public class IvoryDatabase{
     public boolean EXISTS(String attribute_name, String attribute_value){
         return false;
     } // FIND()
+
+    /**
+     * The close() method will run a loop through the attributes[] array and
+     * assign every Attribute object as 'null'. This will be mark the objects 
+     * for garbage collection. 
+     * Then System.gc() is invoked to call the JVM Garbage Collector which
+     * will clean up the objects and free up system resources.
+     */
+    @Override
+    public void close() { 
+        for(Attribute attribute:attributes) { 
+            
+            attribute = null; 
+        }
+        System.gc(); // invoking java garbage collector
+    } // close()
 } // class
