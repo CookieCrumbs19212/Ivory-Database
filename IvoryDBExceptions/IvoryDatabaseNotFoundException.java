0d6b63caf6dd23d16f3ec05eefc0c2be07d78513
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-05-28 01:31:50 +0530

    Exception folder clean up
    deleted NullArgumentException, it was redundant.
    
    edited parameter of IvoryDatabaseNotFoundException from location to file_path.

--- IvoryDBExceptions/IvoryDatabaseNotFoundException.java
+++ IvoryDBExceptions/IvoryDatabaseNotFoundException.java
@@ -1,7 +1,7 @@
 package IvoryDBExceptions;
 
 public class IvoryDatabaseNotFoundException extends Exception {
-    public IvoryDatabaseNotFoundException(String location) {
-        super("Ivory Database file (.ivry) is not found in: " + location);
+    public IvoryDatabaseNotFoundException(String file_path) {
+        super("Ivory Database file (.ivry) is not found in: " + file_path);
     }
 }
--- IvoryDBExceptions/NullArgumentException.java
+++
@@ -1,7 +0,0 @@
-package IvoryDBExceptions;
-
-public class NullArgumentException extends Exception {
-    public NullArgumentException(String var_name) {
-        super("Null value argument passed for String \"" + var_name + "\".");
-    }
-}
