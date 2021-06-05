7237c411190bf702d282abcba5d864a5054a2a10
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-06-06 02:19:16 +0530

    Renamed AttributeNotFoundException to ColumnNotFoundException

--- IvoryDatabase/IvoryDBExceptions/ColumnNotFoundException.java
+++ IvoryDatabase/IvoryDBExceptions/ColumnNotFoundException.java
@@ -1,7 +1,7 @@
 package IvoryDatabase.IvoryDBExceptions;
 
-public class AttributeNotFoundException extends Exception {
-    public AttributeNotFoundException(String attribute_name, String DB_name) {
+public class ColumnNotFoundException extends Exception {
+    public ColumnNotFoundException(String attribute_name, String DB_name) {
         super("The attribute \"" + attribute_name + "\" is not found in Ivory Database \"" + DB_name + "\".");
     }
 }
