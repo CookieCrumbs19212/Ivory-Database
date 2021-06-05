476474d1e724abe13649f3057a8a332989a966b1
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-06-06 03:24:14 +0530

    Delete UnsupportedOperationException.java

--- IvoryDatabase/IvoryDBExceptions/UnsupportedOperationException.java
+++
@@ -1,7 +0,0 @@
-package IvoryDatabase.IvoryDBExceptions;
-
-public class UnsupportedOperationException extends Exception {
-    public UnsupportedOperationException(String DB_name) {
-        super("Unsupported Operation attempted on Ivory Database \"" + DB_name + "\".");
-    }
-}
