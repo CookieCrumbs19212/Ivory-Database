73923d2a55278ff6cf4abea91313cabe96075dc2
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-06-07 17:58:36 +0530

    Delete IvoryDatabaseNotFoundException.java

--- IvoryDatabase/IvoryDBExceptions/IvoryDatabaseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package IvoryDatabase.IvoryDBExceptions;
-
-public class IvoryDatabaseNotFoundException extends Exception {
-    public IvoryDatabaseNotFoundException(String file_path) {
-        super("Ivory Database file (.ivry) is not found in: " + file_path);
-    }
-}
